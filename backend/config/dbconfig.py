5a897fc62f77f7ecc21afcac6a45848ac345f1b0
josejavi3009 <jose.vazquez38@upr.edu>
2022-03-17 02:03:40 -0400

    added dbconfig for db credentials and connection

---
+++ backend/config/dbconfig.py
@@ -0,0 +1,7 @@
+pg_config = {
+    'user': 'xnotibvfjsshlw',
+    'password': 'b27fdcd0087e85c8980971741016230fa8fc6e10eb0ab5f60d724a8d4439513d',
+    'dbname': 'd7n40sl2lek8js',
+    'dbport': 5432,
+    'host': 'ec2-34-224-226-38.compute-1.amazonaws.com'
+}
